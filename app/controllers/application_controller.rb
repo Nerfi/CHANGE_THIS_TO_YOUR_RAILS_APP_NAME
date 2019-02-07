8e83bd19076615cea1119386ad0cf97192b619b0
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-07 18:41:39 +0100

    bye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
+#adding first name and last name to sign_up
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  #whitelisting users
+  before_action :authenticate_user!
   #protegiendo la app =)
   protect_from_forgery with: :exception
 
